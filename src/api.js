7754d216ab1c089a47b9745759fc47b9f8ac42f8
Alan Bedoya <alanjbedoya@gmail.com>
2021-05-25 03:43:22 -0700

    Adding my api url paths.

--- src/api.js
+++ src/api.js
@@ -1,3 +1,37 @@
-const base_url = 'https://api.rawg.io/api';
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
 
-const popular_games =
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const baseUrl = 'https://api.rawg.io/api/';
+const key = `key=${process.env.API_KEY}`;
+
+const popularGames = `games?${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=12`;
+const upcomingGames = `games?${key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=12`;
+const newGames = `games?${key}&dates=${currentDate},${nextYear}&ordering=-released&page_size=12`;
+
+const popularGamesURL = () => `${baseUrl}${popularGames}`;
+const upcomingGamesURL = () => `${baseUrl}${upcomingGames}`;
+const newGamesURL = () => `${baseUrl}${newGames}`;
+
+console.log(popularGamesURL());
