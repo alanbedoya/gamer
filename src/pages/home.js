daba5ab2f699ef0a7e4705d24c95a84275ad1a03
Alan Bedoya <alanjbedoya@gmail.com>
2021-06-01 04:51:22 -0700

    Moved Upcoming Games to the top of the homepage.

--- src/pages/home.js
+++ src/pages/home.js
@@ -27,6 +27,19 @@ const Home = () => {
             </motion.div>
           )}
         </AnimatePresence>
+        <h2>Upcoming Games</h2>
+        <motion.div className='games'>
+          {upcoming &&
+            upcoming.map((game) => (
+              <Game
+                name={game.name}
+                released={game.released}
+                id={game.id}
+                image={game.background_image}
+                key={game.id}
+              />
+            ))}
+        </motion.div>
         <h2>Popular Games</h2>
         <motion.div className='games'>
           {popular &&
@@ -44,19 +57,6 @@ const Home = () => {
         <motion.div className='games'>
           {newGames &&
             newGames.map((game) => (
-              <Game
-                name={game.name}
-                released={game.released}
-                id={game.id}
-                image={game.background_image}
-                key={game.id}
-              />
-            ))}
-        </motion.div>
-        <h2>Upcoming Games</h2>
-        <motion.div className='games'>
-          {upcoming &&
-            upcoming.map((game) => (
               <Game
                 name={game.name}
                 released={game.released}
